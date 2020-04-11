d5ffe50c8559e03da17b0a7e44243dfe58b9ae7c
fabio <fabiotrujillo03@gmail.com>
2020-04-11 18:00:32 -0500

    agregar Jquery 3.5.0

--- index.php
+++ index.php
@@ -2,10 +2,17 @@
 <html>
 <head>
 	<title>Fabio Trujillo Gomez</title>
+<script type="text/javascript" src="libreria/jquery.js"></script>
 </head>
 <body>
-	<h1>Esta Es la segunda reunion<h1>
-
-
+<h1>Esta es la segunda reunion</h1>
+<div id="contenedor">
+Clik Aqui
+<div>
 </body>
+<script type="text/javascript">
+$("#contenedor").click(function(){
+	console.log("Hizo Clik");
+ });
+</script>
 </html>
