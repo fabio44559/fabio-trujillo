fa741e40fbff4d4c230898bb197d510018d3621d
fabio44559 <62025324+fabio44559@users.noreply.github.com>
2020-05-13 17:38:30 -0500

    Delete configurar.php

--- configurar.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-//ruta de la aplicacion
-define('RUTA_APP',dirname(dirname( __FILE__));
-//Ruta Url ejemplo http//localhost/mvc/app/
-define('RUTA_Url',  http//localhost/mvc/app/);
-define('_NOMBRE_SITIO', '_NOMBRE_SITIO')
